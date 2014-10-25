89ed41b84c2e9b0fef22f759176ce75fe5c3bdf1
luohong722 <442666876@qq.com>
2014-10-26 00:25:42 +0800

    添加启动页面

---
+++ FatWaterScale/src/com/powcanscale/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.powcanscale;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+	private final int SPLASH_DISPLAY_LENGHT = 3000; // —”≥Ÿ»˝√Î
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(mainIntent);
+				finish();
+			}
+
+		}, SPLASH_DISPLAY_LENGHT);
+	}
+
+}

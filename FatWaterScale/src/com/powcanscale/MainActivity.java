5943da1dfefe656935a5d81b41b5ba2cd4bb8ed9
luohong722 <442666876@qq.com>
2014-10-25 23:39:55 +0800

    支持社交网络SSO登录和分享功能

--- FatWaterScale/src/com/powcanscale/MainActivity.java
+++ FatWaterScale/src/com/powcanscale/MainActivity.java
@@ -1,6 +1,7 @@
 package com.powcanscale;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +18,14 @@ import android.view.ViewGroup;
 
 import com.powcanscale.adapter.SectionsPagerAdapter;
 import com.umeng.analytics.MobclickAgent;
+import com.umeng.socialize.controller.UMServiceFactory;
+import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.QZoneSsoHandler;
+import com.umeng.socialize.sso.SinaSsoHandler;
+import com.umeng.socialize.sso.UMQQSsoHandler;
+import com.umeng.socialize.sso.UMSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
 import com.umeng.update.UmengUpdateAgent;
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -32,6 +41,8 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 	 * {@link #restoreActionBar()}.
 	 */
 	private CharSequence mTitle;
+	
+	private UMSocialService mController;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,34 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 		setContentView(R.layout.activity_main);
 		UmengUpdateAgent.update(this);
 		MobclickAgent.updateOnlineConfig(this);
+		
+
+		// 首先在您的Activity中添加如下成员变量
+		mController = UMServiceFactory.getUMSocialService("com.umeng.share");
+		// 设置分享内容
+		mController.setShareContent("友盟社会化组件（SDK）让移动应用快速整合社交分享功能，http://www.umeng.com/social");
+		// 设置分享图片, 参数2为图片的url地址
+		mController.setShareMedia(new UMImage(this, 
+		                                      "http://www.umeng.com/images/pic/banner_module_social.png"));
+		// wx967daebe835fbeac是你在微信开发平台注册应用的AppID, 这里需要替换成你注册的AppID
+		String appID = "wx967daebe835fbeac";
+		// 添加微信平台
+		UMWXHandler wxHandler = new UMWXHandler(this, appID);
+		wxHandler.addToSocialSDK();
+		// 支持微信朋友圈
+		UMWXHandler wxCircleHandler = new UMWXHandler(this, appID);
+		wxCircleHandler.setToCircle(true);
+		wxCircleHandler.addToSocialSDK();
+		//设置新浪SSO handler
+		mController.getConfig().setSsoHandler(new SinaSsoHandler());
+		//参数1为当前Activity，参数2为开发者在QQ互联申请的APP ID，参数3为开发者在QQ互联申请的APP kEY.
+		UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(this, "100424468",
+		                "c7394704798a158208a74ab60104f0ba");
+		qqSsoHandler.addToSocialSDK();  
+		//参数1为当前Activity，参数2为开发者在QQ互联申请的APP ID，参数3为开发者在QQ互联申请的APP kEY.
+		QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(this, "100424468",
+		                "c7394704798a158208a74ab60104f0ba");
+		qZoneSsoHandler.addToSocialSDK();
 
 		mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
 		mTitle = getTitle();
@@ -95,6 +134,8 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+			// 是否只有已登录用户才能打开分享选择页
+	        mController.openShare(this, false);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -172,6 +213,16 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
 			super.onAttach(activity);
 			((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
 		}
+	}
+	
+	@Override 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    /**使用SSO授权必须添加如下代码 */
+	    UMSsoHandler ssoHandler = mController.getConfig().getSsoHandler(requestCode) ;
+	    if(ssoHandler != null){
+	       ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+	    }
 	}
 
 }
